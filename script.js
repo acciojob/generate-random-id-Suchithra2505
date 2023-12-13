485768d63aa3595c145e4c5bc790fef4043160b1
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-13 14:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var text="";
+	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for(var i=0;i<l;i++){
+		text+=char_list.charAt(
+			Math.floor(
+				Math.random()* char_list.length
+			)
+		)
+	}
+	return text;
 }
 
 // Do not change the code below.
